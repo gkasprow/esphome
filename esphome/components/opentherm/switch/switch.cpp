ed8ad93d7bfaf612b0bde774bb69b41c325fb5e3
Oleg Tarasov <me@olegtarasov.email>
2024-11-06 15:23:23 +0300

    More fixes from code review

--- esphome/components/opentherm/switch/switch.cpp
+++ esphome/components/opentherm/switch/switch.cpp
@@ -13,16 +13,15 @@ void OpenthermSwitch::setup() {
   if (!restored.has_value()) {
     ESP_LOGD(TAG, "Couldn't restore state for OpenTherm switch '%s'", this->get_name().c_str());
   } else {
-    ESP_LOGD(TAG, "Restored state for OpenTherm switch '%s': %d", this->get_name().c_str(),
-             restored.value());
+    ESP_LOGD(TAG, "Restored state for OpenTherm switch '%s': %d", this->get_name().c_str(), restored.value());
     state = restored.value();
   }
   this->write_state(state);
 }
 
 void OpenthermSwitch::dump_config() {
-  LOG_SWITCH(TAG, "", "OpenTherm Switch", this);
-  ESP_LOGCONFIG("opentherm.switch", "  Current state: %d", this->state);
+  LOG_SWITCH("", "OpenTherm Switch", this);
+  ESP_LOGCONFIG(TAG, "  Current state: %d", this->state);
 }
 
 }  // namespace opentherm
