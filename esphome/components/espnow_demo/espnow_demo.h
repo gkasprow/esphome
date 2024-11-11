0c9ebe8054d505aa277d8539ee68ba32dc520174
NP v/d Spek <github_mail@lumensoft.nl>
2024-11-11 22:56:32 +0100

    start of demo espnow component

---
+++ esphome/components/espnow_demo/espnow_demo.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#if defined(USE_ESP32)
+
+#include "esphome/core/automation.h"
+#include "esphome/core/component.h"
+#include "esphome/core/helpers.h"
+#include <esp_now.h>
+
+#include <array>
+#include <memory>
+#include <queue>
+#include <vector>
+#include <mutex>
+#include <map>
+
+namespace esphome {
+namespace espnow {}  // namespace espnow
+}  // namespace esphome
+
+#endif
