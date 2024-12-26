52b9a9c359329d04d47670bcb8c8f957eff10e9c
Kevin Ahrendt <kevin.ahrendt@nabucasa.com>
2024-12-26 19:23:12 +0000

    run update function in a task

--- esphome/components/http_request/update/http_request_update.cpp
+++ esphome/components/http_request/update/http_request_update.cpp
@@ -9,6 +9,13 @@
 namespace esphome {
 namespace http_request {
 
+// The update function runs in a task only on ESP32s.
+#ifdef USE_ESP32
+#define UPDATE_RETURN vTaskDelete(nullptr)  // Delete the current update task
+#else
+#define UPDATE_RETURN return
+#endif
+
 static const char *const TAG = "http_request.update";
 
 static const size_t MAX_READ_SIZE = 256;
@@ -29,28 +36,37 @@ void HttpRequestUpdate::setup() {
 }
 
 void HttpRequestUpdate::update() {
-  auto container = this->request_parent_->get(this->source_url_);
+#ifdef USE_ESP32
+  xTaskCreate(HttpRequestUpdate::update_task, "update_task", 4096, (void *) this, 1, &this->update_task_handle_);
+#else
+  this->update_task(this);
+#endif
+}
+
+void HttpRequestUpdate::update_task(void *params) {
+  HttpRequestUpdate *this_update = (HttpRequestUpdate *) params;
+
+  auto container = this_update->request_parent_->get(this_update->source_url_);
 
   if (container == nullptr || container->status_code != HTTP_STATUS_OK) {
-    std::string msg = str_sprintf("Failed to fetch manifest from %s", this->source_url_.c_str());
-    this->status_set_error(msg.c_str());
-    return;
+    std::string msg = str_sprintf("Failed to fetch manifest from %s", this_update->source_url_.c_str());
+    this_update->status_set_error(msg.c_str());
+    UPDATE_RETURN;
   }
 
   ExternalRAMAllocator<uint8_t> allocator(ExternalRAMAllocator<uint8_t>::ALLOW_FAILURE);
   uint8_t *data = allocator.allocate(container->content_length);
   if (data == nullptr) {
     std::string msg = str_sprintf("Failed to allocate %d bytes for manifest", container->content_length);
-    this->status_set_error(msg.c_str());
+    this_update->status_set_error(msg.c_str());
     container->end();
-    return;
+    UPDATE_RETURN;
   }
 
   size_t read_index = 0;
   while (container->get_bytes_read() < container->content_length) {
     int read_bytes = container->read(data + read_index, MAX_READ_SIZE);
 
-    App.feed_wdt();
     yield();
 
     read_index += read_bytes;
@@ -61,13 +77,13 @@ void HttpRequestUpdate::update() {
 
   container->end();
 
-  bool valid = json::parse_json(response, [this](JsonObject root) -> bool {
+  bool valid = json::parse_json(response, [this_update](JsonObject root) -> bool {
     if (!root.containsKey("name") || !root.containsKey("version") || !root.containsKey("builds")) {
       ESP_LOGE(TAG, "Manifest does not contain required fields");
       return false;
     }
-    this->update_info_.title = root["name"].as<std::string>();
-    this->update_info_.latest_version = root["version"].as<std::string>();
+    this_update->update_info_.title = root["name"].as<std::string>();
+    this_update->update_info_.latest_version = root["version"].as<std::string>();
 
     for (auto build : root["builds"].as<JsonArray>()) {
       if (!build.containsKey("chipFamily")) {
@@ -84,13 +100,13 @@ void HttpRequestUpdate::update() {
           ESP_LOGE(TAG, "Manifest does not contain required fields");
           return false;
         }
-        this->update_info_.firmware_url = ota["path"].as<std::string>();
-        this->update_info_.md5 = ota["md5"].as<std::string>();
+        this_update->update_info_.firmware_url = ota["path"].as<std::string>();
+        this_update->update_info_.md5 = ota["md5"].as<std::string>();
 
         if (ota.containsKey("summary"))
-          this->update_info_.summary = ota["summary"].as<std::string>();
+          this_update->update_info_.summary = ota["summary"].as<std::string>();
         if (ota.containsKey("release_url"))
-          this->update_info_.release_url = ota["release_url"].as<std::string>();
+          this_update->update_info_.release_url = ota["release_url"].as<std::string>();
 
         return true;
       }
@@ -99,20 +115,20 @@ void HttpRequestUpdate::update() {
   });
 
   if (!valid) {
-    std::string msg = str_sprintf("Failed to parse JSON from %s", this->source_url_.c_str());
-    this->status_set_error(msg.c_str());
-    return;
+    std::string msg = str_sprintf("Failed to parse JSON from %s", this_update->source_url_.c_str());
+    this_update->status_set_error(msg.c_str());
+    UPDATE_RETURN;
   }
 
-  // Merge source_url_ and this->update_info_.firmware_url
-  if (this->update_info_.firmware_url.find("http") == std::string::npos) {
-    std::string path = this->update_info_.firmware_url;
+  // Merge source_url_ and this_update->update_info_.firmware_url
+  if (this_update->update_info_.firmware_url.find("http") == std::string::npos) {
+    std::string path = this_update->update_info_.firmware_url;
     if (path[0] == '/') {
-      std::string domain = this->source_url_.substr(0, this->source_url_.find('/', 8));
-      this->update_info_.firmware_url = domain + path;
+      std::string domain = this_update->source_url_.substr(0, this_update->source_url_.find('/', 8));
+      this_update->update_info_.firmware_url = domain + path;
     } else {
-      std::string domain = this->source_url_.substr(0, this->source_url_.rfind('/') + 1);
-      this->update_info_.firmware_url = domain + path;
+      std::string domain = this_update->source_url_.substr(0, this_update->source_url_.rfind('/') + 1);
+      this_update->update_info_.firmware_url = domain + path;
     }
   }
 
@@ -123,19 +139,22 @@ void HttpRequestUpdate::update() {
   current_version = ESPHOME_VERSION;
 #endif
 
-  this->update_info_.current_version = current_version;
+  this_update->update_info_.current_version = current_version;
 
-  if (this->update_info_.latest_version.empty() || this->update_info_.latest_version == update_info_.current_version) {
-    this->state_ = update::UPDATE_STATE_NO_UPDATE;
+  if (this_update->update_info_.latest_version.empty() ||
+      this_update->update_info_.latest_version == this_update->update_info_.current_version) {
+    this_update->state_ = update::UPDATE_STATE_NO_UPDATE;
   } else {
-    this->state_ = update::UPDATE_STATE_AVAILABLE;
+    this_update->state_ = update::UPDATE_STATE_AVAILABLE;
   }
 
-  this->update_info_.has_progress = false;
-  this->update_info_.progress = 0.0f;
+  this_update->update_info_.has_progress = false;
+  this_update->update_info_.progress = 0.0f;
 
-  this->status_clear_error();
-  this->publish_state();
+  this_update->status_clear_error();
+  this_update->publish_state();
+
+  UPDATE_RETURN;
 }
 
 void HttpRequestUpdate::perform(bool force) {
