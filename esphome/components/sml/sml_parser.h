443e642d9fb258a393ea2783b3c6c7acbb1ed9af
Unai Uribarri <unaiur@gmail.com>
2025-01-13 17:14:48 +0100

    fix: reduce update latencies in sml component
    Sml running in a ESP32-S2 is issuing constant warnings about high latencies:
    
    ```
    [17:13:31][W][component:237]: Component sml took a long time for an operation (131 ms).
    [17:13:31][W][component:238]: Components should block for at most 30 ms.
    ```
    
    This changes replaces vectors with spans (C++20) and eliminates multiple memory allocations,
    reducing the latencies from 120-130ms to 50-60ms.
    
    Since most frameworks are still using C++11, it also includes a span backport
    from https://github.com/tcbrindle/span.

--- esphome/components/sml/sml_parser.h
+++ esphome/components/sml/sml_parser.h
@@ -5,50 +5,55 @@
 #include <string>
 #include <vector>
 #include "constants.h"
+#include "span.h"
 
 namespace esphome {
 namespace sml {
 
 using bytes = std::vector<uint8_t>;
+using byte_span = tcb::span<uint8_t>;
 
 class SmlNode {
  public:
-  uint8_t type;
-  bytes value_bytes;
-  std::vector<SmlNode> nodes;
+  SmlNode(uint8_t type_, byte_span bytes_) : type(type_), value_bytes(bytes_) {}
+  SmlNode(uint8_t type_, std::vector<SmlNode> &&nodes_) : type(type_), nodes(nodes_) {}
+
+  const uint8_t type;
+  const byte_span value_bytes;
+  const std::vector<SmlNode> nodes;
 };
 
 class ObisInfo {
  public:
-  ObisInfo(bytes server_id, SmlNode val_list_entry);
-  bytes server_id;
-  bytes code;
-  bytes status;
+  ObisInfo(byte_span const &server_id, SmlNode const &val_list_entry);
+  byte_span server_id;
+  byte_span code;
+  byte_span status;
   char unit;
   char scaler;
-  bytes value;
+  byte_span value;
   uint16_t value_type;
   std::string code_repr() const;
 };
 
 class SmlFile {
  public:
-  SmlFile(bytes buffer);
-  bool setup_node(SmlNode *node);
+  SmlFile(byte_span const &buffer);
+  bool setup_node(std::vector<SmlNode> &nodes);
   std::vector<SmlNode> messages;
   std::vector<ObisInfo> get_obis_info();
 
  protected:
-  const bytes buffer_;
+  const byte_span buffer_;
   size_t pos_;
 };
 
-std::string bytes_repr(const bytes &buffer);
+std::string bytes_repr(const byte_span &buffer);
 
-uint64_t bytes_to_uint(const bytes &buffer);
+uint64_t bytes_to_uint(const byte_span &buffer);
 
-int64_t bytes_to_int(const bytes &buffer);
+int64_t bytes_to_int(const byte_span &buffer);
 
-std::string bytes_to_string(const bytes &buffer);
+std::string bytes_to_string(const byte_span &buffer);
 }  // namespace sml
 }  // namespace esphome
