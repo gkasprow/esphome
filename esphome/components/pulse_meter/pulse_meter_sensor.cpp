c1607e88c0df09bac4a94e41e7018cbc5b116ffa
Djordje Mandic <6750655+DjordjeMandic@users.noreply.github.com>
2025-01-22 14:29:37 +0100

    Update pulse_meter_sensor.cpp

--- esphome/components/pulse_meter/pulse_meter_sensor.cpp
+++ esphome/components/pulse_meter/pulse_meter_sensor.cpp
@@ -75,7 +75,7 @@ void PulseMeterSensor::loop() {
       case MeterState::RUNNING: {
         uint32_t delta_us = this->get_->last_detected_edge_us_ - this->last_processed_edge_us_;
         float pulse_width_us = delta_us / float(this->get_->count_);
-        ESP_LOGV(TAG, "New pulse, delta: " PRIu32 " us, count: " PRIu32 ", width: %.5f us", delta_us,
+        ESP_LOGV(TAG, "New pulse, delta: %" PRIu32 " µs, count: %" PRIu32 ", width: %.5f µs", delta_us,
                  this->get_->count_, pulse_width_us);
         this->publish_state((60.0f * 1000000.0f) / pulse_width_us);
       } break;
