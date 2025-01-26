2e84d53c7eff63df8c1435379523f5f189329f9a
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-01-26 03:41:45 +0100

    [HX711] Add power actions

--- esphome/components/hx711/sensor.py
+++ esphome/components/hx711/sensor.py
@@ -1,8 +1,14 @@
-from esphome import pins
+from esphome import automation, pins
 import esphome.codegen as cg
 from esphome.components import sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_CLK_PIN, CONF_GAIN, ICON_SCALE, STATE_CLASS_MEASUREMENT
+from esphome.const import (
+    CONF_CLK_PIN,
+    CONF_GAIN,
+    CONF_ID,
+    ICON_SCALE,
+    STATE_CLASS_MEASUREMENT,
+)
 
 hx711_ns = cg.esphome_ns.namespace("hx711")
 HX711Sensor = hx711_ns.class_("HX711Sensor", sensor.Sensor, cg.PollingComponent)
@@ -17,6 +23,15 @@ GAINS = {
     32: HX711Gain.HX711_GAIN_32,
     64: HX711Gain.HX711_GAIN_64,
 }
+
+PowerUpAction = hx711_ns.class_("PowerUpAction", automation.Action)
+PowerDownAction = hx711_ns.class_("PowerDownAction", automation.Action)
+
+HX711_ACTION_BASE_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(CONF_ID): cv.use_id(HX711Sensor),
+    }
+)
 
 CONFIG_SCHEMA = (
     sensor.sensor_schema(
@@ -52,3 +67,22 @@ async def to_code(config):
     cg.add(var.set_gain(config[CONF_GAIN]))
     cg.add(var.set_settling_time_ms(config[CONF_SETTLING_TIME]))
     cg.add(var.set_power_down_after_reading(config[CONF_POWER_DOWN_AFTER_READING]))
+
+
+async def hx711_action_to_code(config, action_id, template_arg):
+    hx711_ = await cg.get_variable(config[CONF_ID])
+    var = cg.new_Pvariable(action_id, template_arg)
+    cg.add(var.set_parent(hx711_))
+    return var
+
+
+@automation.register_action("hx711.power_up", PowerUpAction, HX711_ACTION_BASE_SCHEMA)
+async def hx711_power_up_to_code(config, action_id, template_arg, args):
+    return await hx711_action_to_code(config, action_id, template_arg)
+
+
+@automation.register_action(
+    "hx711.power_down", PowerDownAction, HX711_ACTION_BASE_SCHEMA
+)
+async def hx711_power_down_to_code(config, action_id, template_arg, args):
+    return await hx711_action_to_code(config, action_id, template_arg)
