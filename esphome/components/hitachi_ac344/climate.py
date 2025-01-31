9b3703e9f0d3bc01a2a90a1dfdde0e6a0e4c0a7a
jsjiang <js4jiang5@users.noreply.github.com>
2025-01-31 13:29:08 +0800

    rename constant to resolve conflict

--- esphome/components/hitachi_ac344/climate.py
+++ esphome/components/hitachi_ac344/climate.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate_ir
-from esphome.const import CONF_ID CONF_TEMPERATURE
+from esphome.const import CONF_ID CONF_TEMPERATURE CONF_FAN_MODE
 
 AUTO_LOAD = ["climate_ir"]
 
@@ -9,7 +9,7 @@ climate_ns = cg.esphome_ns.namespace("climate")
 hitachi_ac344_ns = cg.esphome_ns.namespace("hitachi_ac344")
 HitachiClimate = hitachi_ac344_ns.class_("HitachiClimate", climate_ir.ClimateIR)
 
-CONF_HORIZONTAL_DEFAULT = "horizontal_default"
+CONF_HORIZONTAL_POSITION = "horizontal_position"
 HORIZONTAL_DIRECTIONS = {
     "left_max": hitachi_ac344_ns.HITACHI_AC344_SWINGH_LEFT_MAX,
     "left": hitachi_ac344_ns.HITACHI_AC344_SWINGH_LEFT,
@@ -34,7 +34,6 @@ HITACHI_FAN_MODES = {
 }
 
 CONF_SWING = "swing"
-CONF_FAN_MODE = "fan_mode"
 CONF_CUSTOM_COOL = "custom_cool"
 CUSTOM_COOL_SCHEMA = cv.Schema(
     {
@@ -75,7 +74,7 @@ CONF_MILDEWPROOF = "mildewproof"
 CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(HitachiClimate),
-        cv.Optional(CONF_HORIZONTAL_DEFAULT, default="middle"): cv.enum(
+        cv.Optional(CONF_HORIZONTAL_POSITION, default="middle"): cv.enum(
             HORIZONTAL_DIRECTIONS
         ),
         cv.Optional(CONF_MILDEWPROOF, default=False): cv.boolean,
@@ -90,7 +89,7 @@ CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await climate_ir.register_climate_ir(var, config)
-    cg.add(var.set_horizontal_default(config[CONF_HORIZONTAL_DEFAULT]))
+    cg.add(var.set_horizontal_default(config[CONF_HORIZONTAL_POSITION]))
     cg.add(var.set_mildewproof(config[CONF_MILDEWPROOF]))
 
     if CONF_CUSTOM_COOL in config:
